0b1ad7769cfb65318fda7ee93abc11317301b02c
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-06-07 17:35:24 +0200

    Write bindings for complete hello world example

--- examples/hello-world/hello-world.scm
+++ examples/hello-world/hello-world.scm
@@ -1,8 +1,7 @@
 (use (prefix glfw3 glfw:)
      (prefix opengl-glew gl:)
-     (prefix nuklear nk:))
-
-(import (prefix nuklear-glfw-opengl2 backend:))
+     (prefix nuklear nk:)
+     (prefix nuklear-glfw-opengl2 backend:))
 
 (define width 250)
 (define height 150)
@@ -12,19 +11,41 @@
 (glfw:make-context-current (glfw:window))
 (set!-values (width height) (glfw:get-window-size (glfw:window)))
 
-(define ctx (backend:init! (glfw:window) #t))
-(backend:init-fonts! (backend:make-font "ProggyClean.ttf" 12))
+(define context (backend:init! (glfw:window) #t))
+(backend:init-fonts!)
 
 (define quit? #f)
+(define show-greeting? #f)
 
 (let loop ()
   (when (and (not (glfw:window-should-close (glfw:window))) (not quit?))
     (glfw:poll-events)
     (backend:new-frame)
 
-    ;; TODO: nk example code
+    (when (nk:window-begin context (nk:make-panel) "Hello World!"
+                           (nk:make-rect 10 10 192 100)
+                           '(border no-scrollbar movable))
+      (nk:layout-row-dynamic context 30 2)
+      (when (nk:button-label context "Click Me!")
+        (printf "Yay\n")
+        (set! show-greeting? #t))
+      (when (nk:button-label context "Quit")
+        (set! quit? #t))
 
-    ;; TODO: nk bg color set
+      (when show-greeting?
+        (if (nk:popup-begin context (nk:make-panel) #f "Greeting" '(dynamic)
+                            (nk:make-rect 15 50 200 150))
+            (begin
+              (nk:layout-row-dynamic context 25 1)
+              (nk:label context "Hello World!" 'centered)
+              (nk:layout-row-dynamic context 25 1)
+              (when (nk:button-label context "OK")
+                (set! show-greeting? #f)
+                (nk:popup-close context))
+              (nk:popup-end context))
+            (set! show-greeting? #f))))
+    (nk:window-end context)
+
     (let-values (((width height) (glfw:get-window-size (glfw:window))))
       (gl:viewport 0 0 width height))
     (gl:clear gl:+color-buffer-bit+)
--- nuklear.scm
+++ nuklear.scm
@@ -1,7 +1,14 @@
 (module nuklear
-  ()
+  (make-panel make-rect
+   window-begin window-end
+   layout-row-dynamic
+   label
+   button-label
+   popup-begin popup-close popup-end)
 
 (import chicken scheme foreign)
+
+(use lolevel)
 
 ;; TODO: report upstream bug regarding headers (removing not strictly
 ;; necessary ones leads to compilation errors and warnings)
@@ -17,5 +24,145 @@
 #define NK_IMPLEMENTATION
 #include "nuklear.h"
 <#
+
+;;; enums
+
+;; enum nk_panel_flags
+(define NK_WINDOW_BORDER (foreign-value "NK_WINDOW_BORDER" int))
+(define NK_WINDOW_BORDER_HEADER (foreign-value "NK_WINDOW_BORDER_HEADER" int))
+(define NK_WINDOW_MOVABLE (foreign-value "NK_WINDOW_MOVABLE" int))
+(define NK_WINDOW_SCALABLE (foreign-value "NK_WINDOW_SCALABLE" int))
+(define NK_WINDOW_CLOSABLE (foreign-value "NK_WINDOW_CLOSABLE" int))
+(define NK_WINDOW_MINIMIZABLE (foreign-value "NK_WINDOW_MINIMIZABLE" int))
+(define NK_WINDOW_DYNAMIC (foreign-value "NK_WINDOW_DYNAMIC" int))
+(define NK_WINDOW_NO_SCROLLBAR (foreign-value "NK_WINDOW_NO_SCROLLBAR" int))
+(define NK_WINDOW_TITLE (foreign-value "NK_WINDOW_TITLE" int))
+
+(define (window-flag->int flag)
+  (case flag
+    ((border) NK_WINDOW_BORDER)
+    ((border-header) NK_WINDOW_BORDER_HEADER)
+    ((movable) NK_WINDOW_MOVABLE)
+    ((scalable) NK_WINDOW_SCALABLE)
+    ((closable) NK_WINDOW_CLOSABLE)
+    ((minimizable) NK_WINDOW_MINIMIZABLE)
+    ((dynamic) NK_WINDOW_DYNAMIC)
+    ((no-scrollbar) NK_WINDOW_NO_SCROLLBAR)
+    ((title) NK_WINDOW_TITLE)))
+
+(define (window-flags->int flags)
+  (apply bitwise-ior (map window-flag->int flags)))
+
+;; enum nk_button_behavior
+(define NK_BUTTON_DEFAULT (foreign-value "NK_BUTTON_DEFAULT" int))
+(define NK_BUTTON_REPEATER (foreign-value "NK_BUTTON_REPEATER" int))
+
+;; enum nk_popup_type
+(define NK_POPUP_STATIC (foreign-value "NK_POPUP_STATIC" int))
+(define NK_POPUP_DYNAMIC (foreign-value "NK_POPUP_DYNAMIC" int))
+
+;; enum nk_text_alignment
+(define NK_TEXT_LEFT (foreign-value "NK_TEXT_LEFT" int))
+(define NK_TEXT_CENTERED (foreign-value "NK_TEXT_CENTERED" int))
+(define NK_TEXT_RIGHT (foreign-value "NK_TEXT_RIGHT" int))
+
+(define (label-alignment->int flag)
+  (case flag
+    ((left) NK_TEXT_LEFT)
+    ((centered) NK_TEXT_CENTERED)
+    ((right) NK_TEXT_RIGHT)))
+
+;;; typedefs
+
+(define-foreign-type nk_context* (nonnull-c-pointer (struct "nk_context")))
+(define-foreign-type nk_panel* (nonnull-c-pointer (struct "nk_panel")))
+(define-foreign-type nk_rect* (nonnull-c-pointer (struct "nk_rect")))
+(define-foreign-type nk_flags unsigned-int32)
+
+;;; stack-allocation helpers
+
+;; NOTE: used for retrieving backend's context pointer
+(define-record context pointer)
+
+(define-record nk_panel pointer)
+(define-record nk_rect pointer)
+
+(define nk_panel-size (foreign-type-size (struct "nk_panel")))
+(define nk_rect-size (foreign-type-size (struct "nk_rect")))
+
+(define (make-panel) (make-nk_panel (make-locative (make-blob nk_panel-size))))
+(define (make-rect x y w h)
+  (let ((nk_rect* (make-locative (make-blob nk_rect-size))))
+    ((foreign-lambda* void ((nk_rect* rect) (float x) (float y) (float w) (float h))
+       "struct nk_rect *r = rect; r->x = x, r->y = y, r->w = w, r->h = h;")
+     nk_rect* x y w h)
+    (make-nk_rect nk_rect*)))
+
+;;; foreign functions
+
+;; window
+(define nk_begin (foreign-lambda* bool ((nk_context* ctx) (nk_panel* layout) (nonnull-c-string title) (nk_rect* bounds) (nk_flags flags)) "C_return(nk_begin(ctx, layout, title, *bounds, flags));"))
+(define nk_end (foreign-lambda void "nk_end" nk_context*))
+
+;; layout
+(define nk_layout_row_dynamic (foreign-lambda void "nk_layout_row_dynamic" nk_context* float int))
+
+;; widgets
+(define nk_label (foreign-lambda void "nk_label" nk_context* nonnull-c-string nk_flags))
+
+;; buttons
+(define nk_button_label (foreign-lambda bool "nk_button_label" nk_context* nonnull-c-string (enum "nk_button_behavior")))
+
+;; popups
+(define nk_popup_begin (foreign-lambda* bool ((nk_context* ctx) (nk_panel* layout) ((enum "nk_popup_type") type) (nonnull-c-string title) (nk_flags flags) (nk_rect* rect)) "C_return(nk_popup_begin(ctx, layout, type, title, flags, *rect));"))
+(define nk_popup_close (foreign-lambda void "nk_popup_close" nk_context*))
+(define nk_popup_end (foreign-lambda void "nk_popup_end" nk_context*))
+
+;;; API
+
+(define (window-begin context layout title rect flags)
+  (let ((context* (context-pointer context))
+        (layout* (nk_panel-pointer layout))
+        (rect* (nk_rect-pointer rect))
+        (flag (window-flags->int flags)))
+    (nk_begin context* layout* title rect* flag)))
+
+(define (window-end context)
+  (let ((context* (context-pointer context)))
+    (nk_end context*)))
+
+(define (layout-row-dynamic context height columns)
+  (let ((context* (context-pointer context)))
+    (nk_layout_row_dynamic context* height columns)))
+
+(define (label context text alignment)
+  (let ((context* (context-pointer context))
+        (flag (label-alignment->int alignment)))
+    (nk_label context* text flag)))
+
+(define (button-label context text #!optional repeater?)
+  (let ((context* (context-pointer context))
+        (flag (if repeater?
+                  NK_BUTTON_REPEATER
+                  NK_BUTTON_DEFAULT)))
+    (nk_button_label context* text flag)))
+
+(define (popup-begin context layout dynamic? title flags rect)
+  (let ((context* (context-pointer context))
+        (layout* (nk_panel-pointer layout))
+        (type (if dynamic?
+                  NK_POPUP_DYNAMIC
+                  NK_POPUP_STATIC))
+        (flag (window-flags->int flags))
+        (rect* (nk_rect-pointer rect)))
+    (nk_popup_begin context* layout* type title flag rect*)))
+
+(define (popup-close context)
+  (let ((context* (context-pointer context)))
+    (nk_popup_close context*)))
+
+(define (popup-end context)
+  (let ((context* (context-pointer context)))
+    (nk_popup_end context*)))
 
 )
